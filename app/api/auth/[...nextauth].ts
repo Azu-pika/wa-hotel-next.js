28b258546d510cdc6097b0699f768c1446c5d63e
Azumi Shimojo <azushimo5585@gmail.com>
2025-03-18 13:07:53 +0000

    string nani

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -4,8 +4,15 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
+  ]
 });
